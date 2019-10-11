8cccb01d687acd377ec63d00f533d8dc8731371c
diurno <diurno@pc-aula>
2019-10-11 11:32:38 +0200

    actual

--- ejercicios/src/ejercicios/Ejercicio13bucles.java
+++ ejercicios/src/ejercicios/Ejercicio13bucles.java
@@ -5,9 +5,14 @@ import javax.swing.JOptionPane;
 public class Ejercicio13bucles {
 
 	public static void main(String[] args) {
-		int numero = Integer.parseInt(JOptionPane.showInputDialog("escribe un numero"));
-		
-		
+	int suma=0;
+		for(int i =0; i<4;i++ ) {
+			int numero = Integer.parseInt(JOptionPane.showInputDialog("escribe un numero"));
+			if(numero>=10) {
+				suma=numero+suma;
+			}
+		}
+		System.out.println("el resultado de la suma es: " + suma);
 	}
 
 }
