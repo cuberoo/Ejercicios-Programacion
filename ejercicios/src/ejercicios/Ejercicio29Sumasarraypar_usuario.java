d38c0f51fab8cd7260ac9dc029c76afa74147261
diurno <diurno@pc-aula>
2019-10-18 11:33:16 +0200

    nuevo intento

--- ejercicios/src/ejercicios/Ejercicio29Sumasarraypar_usuario.java
+++ ejercicios/src/ejercicios/Ejercicio29Sumasarraypar_usuario.java
@@ -1,22 +1,18 @@
 package ejercicios;
 
-public class Ejercicio28_suma_pares_impares {
+public class Ejercicio29Sumasarraypar_usuario {
 
 	public static void main(String[] args) {
 		int[] array = utils.obtenernumero(150, 0, 100);
 		utils.mostrar_array(array);
 		int sumapar=0;
-		int sumaimpar=0;
 		for (int i = 0; i < array.length; i++) {
 			if(i%2==0) {
 				sumapar += array[i];
 				}
-			else {
-				sumaimpar += array[i];
-			}
 		}
 		System.out.println("\nla suma de los numeros pares es " + sumapar);
-		System.out.println("la suma de los numeros impares es " + sumaimpar);
+		
 	}
 
 }
--- ejercicios/src/ejercicios/utils.java
+++ ejercicios/src/ejercicios/utils.java
@@ -3,12 +3,18 @@ package ejercicios;
 public class utils {
 
 		
-public static int[] obtenernumero () {
-	int array[]= new int[150];
+public static int[] obtenernumero (int max , int numinf, int numsup) {
+	int array[]= new int[max];
 	for (int i = 0; i < array.length; i++) {
-		array[i] = (int) Math.round(Math.random()*100);
+		array[i] = (int) Math.round(Math.random()* (numsup-numinf)+numinf);
 	}
 	return array;
 	}
 
+public static void mostrar_array (int[] array) {
+	System.out.print("el array es: ");
+	for (int i = 0; i < array.length; i++) {
+		System.out.print(array[i] + " ");
+	}
+}
 }
